c2bc2299c59ceb1a08c07a0f6737e1e12f557882
modestemax <modestemax@gmail.com>
2021-09-23 19:41:47 +0100

    first commit

--- config/functions/bootstrap.js
+++ config/functions/bootstrap.js
@@ -10,4 +10,11 @@
  * See more details here: https://strapi.io/documentation/developer-docs/latest/setup-deployment-guides/configurations.html#bootstrap
  */
 
-module.exports = () => {};
+module.exports = () => {
+  strapi.admin.services.permission.conditionProvider.register({
+    displayName: 'Billing amount under 10K',
+    name: 'billing-amount-under-10k',
+    plugin: 'admin',
+    handler: {amount: {$lt: 10000}},
+  });
+};
