0cad94f17353b133c0c7a175495a1d6ed92e80eb
modestemax <modestemax@gmail.com>
2021-10-12 13:06:04 +0100

    send single mail

--- api/alerte/services/alerte.js
+++ api/alerte/services/alerte.js
@@ -16,16 +16,23 @@ module.exports = {
   },
 
   async publish(alert, contacts) {
-    try {
-      console.log('sending email to ',contacts.map(c => ({email: c.Email, name: `${c.Nom} ${c.Prenom}`})))
-      await strapi.plugins.email.services
-        .email.send({
-          to: contacts.map(c => ({email: c.Email, name: `${c.Nom} ${c.Prenom}`})),
-          subject: alert.Title, text: alert.Resume, html: alert.Resume
-        });
-    } catch (ex) {
-      console.log('email error ', ex)
+
+    const to = contacts.map(c => ({email: c.Email, name: `${c.Nom} ${c.Prenom}`}));
+    console.log('sending email to ', to)
+    for (const to1 of to) {
+      try {
+        await strapi.plugins.email.services
+          .email.send({
+            to: [to1],
+            subject: alert.Title,
+            text: alert.Resume,
+            html: alert.Resume
+          });
+      } catch (ex) {
+        console.log('email error ', ex)
+      }
     }
+
 
   }
 
