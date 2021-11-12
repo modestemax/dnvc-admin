4b9ad308bdd857088178a60e308fc92da7659494
llorel <lorelkouam@gmail.com>
2021-11-12 17:37:26 +0100

    Update adv-search

--- api/alerte/controllers/alerte.js
+++ api/alerte/controllers/alerte.js
@@ -6,28 +6,39 @@
  */
 
 module.exports = {
+
   async advSearch(ctx) {
+
     const where = ctx.query._where;
 
-    let query = `select  a.*
+    let select = `select  a.*
                  from alertes a
                         left join alertes__filieres af on a.id = af.alerte_id
                         left join filieres f on af.filiere_id = f.id
                         left join alertes__marches am on a.id = am.alerte_id
                         left join marches m on am.march_id = m.id
                         left join themes_de_veilles tv on a.themes_de_veille = tv.id
-
-                 where (${"Filieres.Name" in where ?'f."Name" = \'' +where ["Filieres.Name"] +'\' or':''} f."Name" is null)
-                   and (${"Marches.Nom" in where ?'m."Nom" = \''+ where ["Marches.Nom"]+ '\' or':''} m."Nom" is null)
-                   and (${"themes_de_veille.Nom" in where ?'tv."Nom" = \''+ where ["themes_de_veille.Nom"] +'\' or':''} tv."Nom" is null)
     `
-    if ("DatePublication_gte" in where)
-      query += ` and ( '${where["DatePublication_gte"]}'  <="DatePublication")`
 
-    if ("DatePublication_lte" in where)
-      query += ` and ( '${where["DatePublication_lte"]}'  >="DatePublication")`
+    const fquery = ("Filieres.Name" in where) ? ` f."Name" = '${where ["Filieres.Name"]}' or f."Name" is null` : 'true'
+
+    const mquery = ("Marches.Nom" in where) ? ` m."Nom" = '${where ["Marches.Nom"]}' or m."Nom" is null` : 'true'
+
+    const tquery = ("themes_de_veille.Nom" in where) ? ` tv."Nom" = '${where ["themes_de_veille.Nom"]}' or tv."Nom" is null` : 'true'
+
+    const dgquery = ("DatePublication_gte" in where) ? `  ( '${where["DatePublication_gte"]}'  <="DatePublication")` : 'true'
+
+    const dlquery = ("DatePublication_lte" in where) ? `  ( '${where["DatePublication_lte"]}'  >="DatePublication")` : 'true'
+
+    const query = `${select} where ${fquery} and ${mquery} and ${tquery} and ${dgquery} and ${dlquery}`
+
+
+    // console.log('query is ', query)
 
     let alertes = await strapi.connections.default.raw(query)
+
+    // console.debug(alertes)
+
     ctx.send(alertes);
 
   }
