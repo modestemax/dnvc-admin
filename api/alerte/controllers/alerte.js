81410a4fc4b64b13588300f6133a77e4dec72812
modestemax <modestemax@gmail.com>
2021-11-13 22:52:08 +0100

    advanced search fix

--- api/alerte/controllers/alerte.js
+++ api/alerte/controllers/alerte.js
@@ -10,43 +10,40 @@ module.exports = {
   async advSearch(ctx) {
 
     const where = ctx.query._where;
+    if (where) {
+      let select = `select a.*, uf.url
+                    from alertes a
+                           left join alertes__filieres af on a.id = af.alerte_id
+                           left join filieres f on af.filiere_id = f.id
+                           left join alertes__marches am on a.id = am.alerte_id
+                           left join marches m on am.march_id = m.id
+                           left join themes_de_veilles tv on a.themes_de_veille = tv.id
+                           left join upload_file_morph ufm on a.id = ufm.related_id
+                           left join upload_file uf on ufm.upload_file_id = uf.id
+      `
 
-    let select = `select  a.*, uf.url
-                 from alertes a
-                        left join alertes__filieres af on a.id = af.alerte_id
-                        left join filieres f on af.filiere_id = f.id
-                        left join alertes__marches am on a.id = am.alerte_id
-                        left join marches m on am.march_id = m.id
-                        left join themes_de_veilles tv on a.themes_de_veille = tv.id
-                        left join upload_file_morph ufm on a.id = ufm.related_id
-                        left join upload_file uf on ufm.upload_file_id = uf.id
-    `
+      const fquery = ("Filieres.Name" in where) ? ` f."Name" = '${where ["Filieres.Name"]}' or f."Name" is null` : 'true'
 
-    const fquery = ("Filieres.Name" in where) ? ` f."Name" = '${where ["Filieres.Name"]}' or f."Name" is null` : 'true'
+      const mquery = ("Marches.Nom" in where) ? ` m."Nom" = '${where ["Marches.Nom"]}' or m."Nom" is null` : 'true'
 
-    const mquery = ("Marches.Nom" in where) ? ` m."Nom" = '${where ["Marches.Nom"]}' or m."Nom" is null` : 'true'
+      const tquery = ("themes_de_veille.Nom" in where) ? ` tv."Nom" = '${where ["themes_de_veille.Nom"]}' or tv."Nom" is null` : 'true'
 
-    const tquery = ("themes_de_veille.Nom" in where) ? ` tv."Nom" = '${where ["themes_de_veille.Nom"]}' or tv."Nom" is null` : 'true'
+      const dgquery = ("DatePublication_gte" in where) ? `  ( '${where["DatePublication_gte"]}'  <="DatePublication")` : 'true'
 
-    const dgquery = ("DatePublication_gte" in where) ? `  ( '${where["DatePublication_gte"]}'  <="DatePublication")` : 'true'
+      const dlquery = ("DatePublication_lte" in where) ? `  ( '${where["DatePublication_lte"]}'  >="DatePublication")` : 'true'
 
-    const dlquery = ("DatePublication_lte" in where) ? `  ( '${where["DatePublication_lte"]}'  >="DatePublication")` : 'true'
-
-    const query = `${select} where ${fquery} and ${mquery} and ${tquery} and ${dgquery} and ${dlquery}`
+      const query = `${select} where ${fquery} and ${mquery} and ${tquery} and ${dgquery} and ${dlquery}`
 
 
-    console.log('query is ', query)
+      console.log('query is ', query)
 
-    let alertes = await strapi.connections.default.raw(query)
+      let alertes = await strapi.connections.default.raw(query)
 
-    alertes.forEach((alerte) => {
-      alerte.sourceFile = [ {"url": alerte.url} ]
-      delete alerte.url
-    })
 
-    console.debug(alertes)
+      console.debug(alertes)
 
-    ctx.send(alertes);
-
+      return ctx.send(alertes && alertes.map(alerte => ({...alerte, sourceFile: [{"url": alerte.url}], url: void 0})));
+    }
+    ctx.badRequest('set conditions')
   }
 };
