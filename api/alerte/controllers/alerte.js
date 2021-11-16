8ed4303a40525caadae27104885342d39af19778
llorel <lorelkouam@gmail.com>
2021-11-16 11:30:45 +0100

    test

--- api/alerte/controllers/alerte.js
+++ api/alerte/controllers/alerte.js
@@ -11,18 +11,18 @@ module.exports = {
 
     const where = ctx.query._where;
     if (where) {
-      let select = `select a.id, a.Title, a.Type, a.Resume, a.DatePublication, a.SourceUrl, group_concat(json_object('Nom', m.Nom), ',') as marches,
-                    group_concat(json_object('url', uf.url), ',') as SourceFile,
-                    group_concat(json_object('NomStructure', sv.NomStructure), ',') as Emetteur
+      let select = `select a."id", a."Title", a."Type", a."Resume", a."DatePublication", a."SourceUrl", group_concat(json_object('Nom', m."Nom"), ',') as marches,
+            group_concat(json_object('url', uf."url"), ',') as SourceFile,
+            group_concat(json_object('NomStructure', sv."NomStructure"), ',') as Emetteur
                     from alertes a
-                           left join alertes__filieres af on a.id = af.alerte_id
-                           left join filieres f on af.filiere_id = f.id
-                           left join alertes__marches am on a.id = am.alerte_id
-                           left join marches m on am.march_id = m.id
-                           left join themes_de_veilles tv on a.themes_de_veille = tv.id
-                           left join structure_de_veilles sv on a."Emetteur" = sv.id
-                           left join upload_file_morph ufm on a.id = ufm.related_id
-                           left join upload_file uf on ufm.upload_file_id = uf.id
+                           left join alertes__filieres af on a."id" = af."alerte_id"
+                           left join filieres f on af."filiere_id" = f."id"
+                           left join alertes__marches am on a."id" = am."alerte_id"
+                           left join marches m on am."march_id" = m."id"
+                           left join themes_de_veilles tv on a."themes_de_veille" = tv."id"
+                           left join structure_de_veilles sv on a."Emetteur" = sv."id"
+                           left join upload_file_morph ufm on a."id" = ufm."related_id"
+                           left join upload_file uf on ufm."upload_file_id" = uf."id"
       `
 
       const fquery = ("Filieres.Name" in where) ? ` f."Name" = '${where ["Filieres.Name"]}' or f."Name" is null` : 'true'
