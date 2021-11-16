f8818f5a638396231afeeb11a8490ae4d6d8c25f
llorel <lorelkouam@gmail.com>
2021-11-16 12:50:33 +0100

    test

--- api/alerte/controllers/alerte.js
+++ api/alerte/controllers/alerte.js
@@ -11,16 +11,18 @@ module.exports = {
 
     const where = ctx.query._where;
     if (where) {
-      let select = `select row_to_json(a)
+      let select = `select a.id, a.Title, a.Type, a.Resume, a.DatePublication, a.SourceUrl, group_concat(json_object('Nom', m.Nom), ',') as marches,
+                    group_concat(uf.url) as SourceFile,
+                    group_concat(sv.NomStructure) as Emetteur
                     from alertes a
-                           left join alertes__filieres af on a."id" = af."alerte_id"
-                           left join filieres f on af."filiere_id" = f."id"
-                           left join alertes__marches am on a."id" = am."alerte_id"
-                           left join marches m on am."march_id" = m."id"
-                           left join themes_de_veilles tv on a."themes_de_veille" = tv."id"
-                           left join structure_de_veilles sv on a."Emetteur" = sv."id"
-                           left join upload_file_morph ufm on a."id" = ufm."related_id"
-                           left join upload_file uf on ufm."upload_file_id" = uf."id"
+                           left join alertes__filieres af on a.id = af.alerte_id
+                           left join filieres f on af.filiere_id = f.id
+                           left join alertes__marches am on a.id = am.alerte_id
+                           left join marches m on am.march_id = m.id
+                           left join themes_de_veilles tv on a.themes_de_veille = tv.id
+                           left join structure_de_veilles sv on a."Emetteur" = sv.id
+                           left join upload_file_morph ufm on a.id = ufm.related_id
+                           left join upload_file uf on ufm.upload_file_id = uf.id
       `
 
       const fquery = ("Filieres.Name" in where) ? ` f."Name" = '${where ["Filieres.Name"]}' or f."Name" is null` : 'true'
@@ -42,13 +44,13 @@ module.exports = {
 
       alertes = alertes.rows
 
-      // alertes.forEach((alerte) => {
-      //   alerte.marches = JSON.parse('[' + alerte.marches + ']')
-      //   alerte.SourceFile = JSON.parse('[' + alerte.SourceFile + ']')[0].url === null ? [] : JSON.parse('[' + alerte.SourceFile + ']')[0]
-      //   alerte.Emetteur = JSON.parse('[' + alerte.Emetteur + ']')[0]
-      //   if (alerte.marches[0].Nom === null)
-      //     alerte.marches = []
-      // })
+      alertes.forEach((alerte) => {
+        alerte.marches = JSON.parse('[' + alerte.marches + ']')
+        alerte.SourceFile = JSON.parse('[' + alerte.SourceFile + ']')[0].url === null ? [] : JSON.parse('[' + alerte.SourceFile + ']')[0]
+        alerte.Emetteur = JSON.parse('[' + alerte.Emetteur + ']')[0]
+        if (alerte.marches[0].Nom === null)
+          alerte.marches = []
+      })
 
       return ctx.send(alertes);
 
