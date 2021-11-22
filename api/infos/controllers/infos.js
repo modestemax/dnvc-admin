7e090de8b14e8c9b0f9a363936140d4b771e5656
LLorel <lorelkoua@gmail.com>
2021-11-22 22:45:37 +0100

    new methods added to infos controler

--- api/infos/controllers/infos.js
+++ api/infos/controllers/infos.js
@@ -5,4 +5,44 @@
  * to customize this controller
  */
 
-module.exports = {};
+module.exports = {
+
+  async getFirstImg(ctx) {
+    let select = `select uf."url" from infos i
+       left join upload_file_morph ufm on i."id" = ufm."related_id"
+       left join upload_file uf on ufm."upload_file_id" = uf."id"
+       where ufm.related_type = 'infos' and ufm.field = 'home_picture1'`;
+
+    let homePictureSRC = await strapi.connections.default.raw(select)
+
+    homePictureSRC = homePictureSRC.rows[0].url
+
+    return ctx.send(homePictureSRC)
+  },
+
+  async getSecondImg(ctx) {
+    let select = `select uf."url" from infos i
+       left join upload_file_morph ufm on i."id" = ufm."related_id"
+       left join upload_file uf on ufm."upload_file_id" = uf."id"
+       where ufm.related_type = 'infos' and ufm.field = 'home_picture2'`;
+
+    let homePictureSRC = await strapi.connections.default.raw(select)
+
+    homePictureSRC = homePictureSRC.rows[0].url
+
+    return ctx.send(homePictureSRC)
+  },
+
+  async getLogoMincom(ctx) {
+    let select = `select uf."url" from infos i
+       left join upload_file_morph ufm on i."id" = ufm."related_id"
+       left join upload_file uf on ufm."upload_file_id" = uf."id"
+       where ufm.related_type = 'infos' and ufm.field = 'logo_mincommerce'`;
+
+    let logo = await strapi.connections.default.raw(select)
+
+    logo = logo.rows[0].url
+
+    return ctx.send(logo)
+  }
+};
